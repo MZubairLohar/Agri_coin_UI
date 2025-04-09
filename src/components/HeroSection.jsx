fec3928514664b12984b395f4ae7b7f1fa45c8ee
Zubair <57180013+MZubairLohar@users.noreply.github.com>
2025-04-09 21:17:52 +0500

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -191,7 +191,7 @@ useEffect(() => {
         <>
         <div
   className="relative min-h-screen flex flex-col items-center text-center px-4 md:px-10 lg:px-20 
-    bg-white bg-cover bg-center bg-no-repeat z-10"
+    bg-white bg-cover bg-center bg-no-repeat z-10 mt-2"
 >
   <Navbar />
           {/*<img
