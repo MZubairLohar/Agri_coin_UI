fe4efc39df6b3662d020a6309a47695b6be39daa
Emad Ali Khan <142682966+EmadAliKhan@users.noreply.github.com>
2025-08-27 14:49:41 +0500

    working

--- src/app/investordash/market/pre-harvest/page.jsx
+++ src/app/investordash/market/pre-harvest/page.jsx
@@ -1,6 +1,39 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
-function Postharvest() {
+function PreHarvest() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      // ✅ API ko call karo
+      const res = await fetch("/api/farmer/preHarvest", {
+        cache: "no-store",
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to fetch data");
+      }
+
+      const result = await res.json();
+      console.log("result", result);
+      // ✅ Sirf pending data filter karo
+      const pendingData = result.data.filter(
+        (item) => item.status !== "pending"
+      );
+
+      setData(pendingData);
+    } catch (error) {
+      console.error("❌ Error fetching preHarvest data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       <h1 className="text-black text-3xl font-bold">Market Pre-Harvest</h1>
@@ -11,4 +44,4 @@ function Postharvest() {
   );
 }
 
-export default Postharvest;
+export default PreHarvest;
